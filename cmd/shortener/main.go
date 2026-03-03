c3cb631868e1cfc21d15fdfd6c43c523aa7b4f66
Евгений Стасенко <evgenijstasenko@MacBook-Pro-Evgenij.local>
2026-03-03 19:02:29 +0300

    use chi

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -4,17 +4,19 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/stasenkoin/URL-ShortenerAI/internal/handler"
 )
 
 func main() {
 	h := handler.New()
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("POST /{$}", h.ShortenURL)
-	mux.HandleFunc("GET /{id}", h.GetURL)
-	mux.HandleFunc("/", h.BadRequest)
+	r := chi.NewRouter()
+	r.Post("/", h.ShortenURL)
+	r.Get("/{id}", h.GetURL)
+	r.NotFound(h.BadRequest)
+	r.MethodNotAllowed(h.BadRequest)
 
 	log.Println("Starting server on :8080")
-	log.Fatal(http.ListenAndServe(":8080", mux))
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/stasenkoin/URL-ShortenerAI
 
 go 1.22
+
+require github.com/go-chi/chi/v5 v5.2.5 // indirect
